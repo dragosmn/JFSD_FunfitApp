22821430a3c1fc8e42612949bf7fe82b8488803d
dragosmn <dragosmn93@gmail.com>
2024-06-03 19:43:23 +0300

    first commit

---
+++ funfitdb.sql
@@ -0,0 +1,7 @@
+create database funfitdb;
+
+use funfitdb;
+
+create table batch(bid int primary key auto_increment, typeofbatch enum('morning','evening'), time varchar(10));
+
+create table participants(pid int primary key auto_increment, fname varchar(30), age int, phonenumber varchar(10), bid int, foreign key(bid) references batch(bid) on delete set null);
